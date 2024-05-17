5aa6c754173c88a3c77f05b2d1cccc51408dd60a
SnugbXReisdencia <santoseverlan45@gmail.com>
2024-05-16 22:22:34 -0300

    add class VerificationToken e jwtProviderService obs fiz uma classe de usuario só para não dar erro!

---
+++ SpringEcommerce/src/main/java/com/ProvaGrupo/SpringEcommerce/entity/VerificationToken.java
@@ -0,0 +1,40 @@
+package com.ProvaGrupo.SpringEcommerce.entity;
+
+import java.time.Instant;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "token")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class VerificationToken {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@NotNull(message = "Token obrigatório")
+	@Column(unique = true, nullable = false)
+	private String token;
+	
+	@NotNull(message = "User obrigatório")
+	private Long userId;
+	
+	@NotNull(message = "ExpiryDate obrigatório")
+	@Column(nullable = false)
+	private Instant expiryDate;
+	
+	
+	
+}
