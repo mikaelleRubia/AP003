38177f459aecc3ab2d7fa25798b87bd87ce26e5a
Dan Souza <darleyble3@gmail.com>
2024-05-21 22:44:46 -0300

    add service

---
+++ SpringEcommerce/src/main/java/com/ProvaGrupo/SpringEcommerce/service/ProductRatingService.java
@@ -0,0 +1,31 @@
+package com.ProvaGrupo.SpringEcommerce.service;
+
+import org.springframework.http.ResponseEntity;
+
+import com.ProvaGrupo.SpringEcommerce.dto.ProductRatingDto;
+
+import jakarta.validation.Valid;
+
+public class ProductRatingService {
+
+	public ResponseEntity<?> postProductRating(@Valid ProductRatingDto productRatingDto) {
+		// TODO Stub de método gerado automaticamente
+		return null;
+	}
+
+	public ResponseEntity<?> editProductRating(@Valid ProductRatingDto productRatingDto) {
+		// TODO Stub de método gerado automaticamente
+		return null;
+	}
+
+	public ResponseEntity<?> deleteProductRating(String ratingId) {
+		// TODO Stub de método gerado automaticamente
+		return null;
+	}
+
+	public Object getProductRating(String sku) {
+		// TODO Stub de método gerado automaticamente
+		return null;
+	}
+
+}
